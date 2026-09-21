0bd1692051085e945d060fc08a38e3f85edefd5b
nitish <nitishkumarsahu411@gmail.com>
2026-09-21 15:50:12 +0530

    update code

--- QR.py
+++ QR.py
@@ -1,7 +1,7 @@
 import qrcode
 
 url= input("Enter Your url: ")
-file_path="C:\\Users\\HP\\Videos\\figma\\Linkdin qr code"
+file_path=" "                    # Enter your file path to store QR code png
 
 qr=qrcode.QRCode()
 qr.add_data(url)
